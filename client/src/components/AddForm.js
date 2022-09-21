f37859c62f605cb235f47c47c9aabb469bd8f823
Anne Tiotuico <amtiotuico@gmail.com>
2022-09-21 16:43:25 -0700

    added add product functionality

--- client/src/components/AddForm.js
+++ client/src/components/AddForm.js
@@ -1,27 +1,58 @@
-const AddForm = () => {
+import { useState } from "react";
+
+const AddForm = ({ onSubmit }) => {
+  const [visible, setVisible] = useState(false);
+  const [product, setProduct] = useState(""); // product
+  const [price, setPrice] = useState(0); // price
+  const [quantity, setQuantity] = useState(0); // quantity; input is a string
+  
+
+  const formClass = visible ? "add-form visible" : "add-form"; 
+
+  const toggleVisibility = e => {
+    setVisible(!visible)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const newProduct = { title: product, price, quantity };
+    onSubmit(newProduct, resetInputs)
+  }
+
+  const resetInputs = () => {
+    setProduct("");
+    setPrice(0);
+    setQuantity(0);
+  };
+
   return (
-    <div class="add-form">
-      <p><a class="button add-product-button">Add A Product</a></p>
+    <div class={formClass}>
+      <p>
+        <a class="button add-product-button" onClick={toggleVisibility}>
+          Add A Product
+        </a>
+      </p>
+
       <h3>Add Product</h3>
-      <form>
+      <form action="" >
         <div class="input-group">
           <label for="product-name">Product Name</label>
-          <input type="text" id="product-name" value="" />
+          <input type="text" id="product-name" value={product} onChange={e => setProduct(e.target.value)}/>
         </div>
 
         <div class="input-group">
           <label for="product-price">Price</label>
-          <input type="text" id="product-price" value=""/>
+          <input type="text" id="product-price" value={price} onChange={e => setPrice(e.target.value)}/>
         </div>
 
         <div class="input-group">
           <label for="product-quantity">Quantity</label>
-          <input type="text" id="product-quantity" value=""/>
+          <input type="text" id="product-quantity" value={quantity} onChange={e => setQuantity(e.target.value)}/>
         </div>
 
         <div class="actions form-actions">
-          <a class="button">Add</a>
-          <a class="button">Cancel</a>
+          <a class="button" onClick={handleSubmit}>Add</a>
+          <a class="button" onClick={toggleVisibility}>Cancel</a>
         </div>
       </form>
     </div>
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,37 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 import ProductListing from "./ProductListing";
 import AddForm from "./AddForm";
-import data from "../../mockData/data";
 
 const App = () => {
-  const [products, setProducts] = useState(data);
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    setProducts(data)
+    const fetchProducts = async () => {
+      const response = await axios.get("/api/products"); 
+      setProducts(response.data)
+    }
+    fetchProducts();
   }, []);
+
+  const handleSubmit = async (newProduct, callback) => {
+    try {
+      const response = await axios.post("/api/products", { ...newProduct });
+      setProducts(products.concat(response.data))
+
+      if (callback) {
+        callback();
+      }
+
+    } catch (e) {
+      console.error(e)
+    }
+  }
 
   return (
     <main>
       <ProductListing products={products}/>
-      <AddForm />
+      <AddForm onSubmit={handleSubmit}/>
     </main>
 )};
 
